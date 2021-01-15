cca49b8c348c14b16c11b6cf7e7a420896c18c95
hikir1 <hikirone@gmail.com>
2021-01-15 15:13:21 -0500

    added helper macros and convenience functions to doc.h

--- doc.h
+++ doc.h
@@ -4,6 +4,25 @@
 #include <assert.h>
 #include "util.h"
 
+	#define DOC_AREA_TYPE(TYPE) typedef struct doc_area_ ## TYPE Doc_area_ ## TYPE;
+
+	#define DOC_AREA(TYPE) \
+	DOC_AREA_TYPE(TYPE) \
+	inline Status doc_add_ ## TYPE (Doc * doc, Doc_area_ ## TYPE * area) { \
+		return doc_add(doc, (Doc_area *) area); \
+	} \
+	Status doc_area_ ## TYPE ## _free(Doc_area_ ## TYPE * area);
+
+	#define DOC_AREA_C(TYPE) \
+	DOC_AREA(TYPE) \
+	Doc_area_ ## TYPE * doc_area_ ## TYPE ## _new(void);
+
+	#define TEXT_AREA(TYPE) \
+	inline Status doc_area_ ## TYPE ## _add_word(Doc_area_ ## TYPE * area, const char * word, size_t len) { \
+		return doc_area_text_add_word((Doc_area_text *) area, word, len); \
+	}
+	
+
 	typedef struct doc Doc;
 	typedef struct doc_area Doc_area;
 
@@ -13,11 +32,10 @@
 	Status doc_add(Doc *, Doc_area *);
 	Status doc_close(Doc *);
 
-	typedef struct doc_area_text Doc_area_text;
+	DOC_AREA_TYPE(text)
 	Status doc_area_text_add_word(Doc_area_text *, const char * word, size_t len);
 
-	typedef struct doc_area_pg Doc_area_pg;
-	Doc_area_pg * doc_area_pg_new();
-	Status doc_area_pg_free(Doc_area_pg *);
+	DOC_AREA_C(pg)
+	TEXT_AREA(pg)
 
 #endif // DOC_H
