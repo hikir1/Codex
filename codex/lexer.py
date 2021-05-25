5f8fffdce37ec5628481e2cc9889d87c9d211f09
hikir1 <hikirone@gmail.com>
2021-05-24 21:09:20 -0400

    lex spaces, words, and newlines

---
+++ codex/lexer.py
@@ -0,0 +1,33 @@
+import sys
+import ply.lex
+
+tokens = (
+	"CHAR",
+	"SPACE",
+	"ENDPARA",
+)
+
+t_CHAR = r"."
+
+def t_ENDPARA(t):
+	r"[ ]*\n[ ]*\n[ ]*(\n[ ]*)*"
+	t.value = None
+	return t
+
+def t_SPACE(t):
+	r"[ \n]+"
+	t.value = None
+	return t
+
+def t_error(t):
+	print(f"ERROR: Illegal character 't.value'")
+	sys.exit(1)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print(f"Usage: {sys.argv[0]} INPUT")
+		sys.exit(1)
+	lexer = ply.lex.lex()
+	lexer.input(sys.argv[1])
+	for tok in lexer:
+		print(tok)
--- test/test-lex.py
+++ test/test-lex.py
@@ -1,72 +1,96 @@
 import unittest
 import ply.lex
-from .context import codex.lexer
 
-class LexerTestCase(unitest.TestCase):
+from context import codex
+import codex.lexer
+
+class LexerTestCase(unittest.TestCase):
 	def setUp(self):
 		self.lexer = ply.lex.lex(module=codex.lexer)
-	def assertToksEqual(self, in, expected):
-		self.lexer.input(in)
+	def assertToksEqual(self, input_text, expected):
+		self.lexer.input(input_text)
 		tok_cnt = 0
-		for tok, exp in zip(lexer, expected):
-			self.assertEqual(tok.type, exp.type)
-			self.assertEqual(tok.value, exp.value)
+		for tok, exp in zip(self.lexer, expected):
+			self.assertEqual(tok.type, exp[0])
+			self.assertEqual(tok.value, exp[1])
 			tok_cnt += 1
-		self.assertEqual(tok_cnt, len(expected))
+		self.assertEqual(tok_cnt, len(expected), "Expected more toks")
+		self.assertFalse(self.lexer.token(), "More toks than expected")
 
 LTC = LexerTestCase
 
+word = "word"
+space_exp = [("SPACE", None)]
+endpara_exp = [("ENDPARA", None)]
+word_exp = [
+	("CHAR", "w"),
+	("CHAR", "o"),
+	("CHAR", "r"),
+	("CHAR", "d"),
+]
+
 class TestSpace(LTC):
 	def test_space(self):
-		self.assertToksEqual(" ", [])
-		self.assertToksEqual("  ", [])
+		self.assertToksEqual(" ", space_exp)
+		self.assertToksEqual("  ", space_exp)
 	def test_newline(self):
-		self.assertToksEqual("\n", [])
-		self.assertToksEqual("\n\n", [])
+		self.assertToksEqual("\n", space_exp)
 	def test_space_and_newline(self):
-		self.assertToksEqual("\n ", [])
-		self.assertToksEqual(" \n", [])
-		self.assertToksEqual(" \n ", [])
-		self.assertToksEqual("\n \n", [])
-		self.assertToksEqual("\n \n\n ", [])
-		self.assertToksEqual(" \n  \n ", [])
+		self.assertToksEqual("\n ", space_exp)
+		self.assertToksEqual(" \n", space_exp)
+		self.assertToksEqual(" \n ", space_exp)
 
 class TestText(LTC):
-	word = "word"
-	eow = [("ENDWORD", "")]
-	wordexp = [
-		("CHAR", "w"),
-		("CHAR", "o"),
-		("CHAR", "r"),
-		("CHAR", "d"),
-		eow,
-	]
 	def test_text_word(self):
-		self.assertToksEqual(word, wordexp + eow)
+		self.assertToksEqual(word, word_exp)
 	def test_text_ascii_alnum(self):
 		ascii_alnum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-		self.assertToksEqual(ascii_alnum, [("CHAR", c) for c in ascii_alnum] + eow)
+		self.assertToksEqual(ascii_alnum, [("CHAR", c) for c in ascii_alnum])
 	def test_text_with_space(self):
-		self.assertToksEqual(" " + word, wordexp)
-		self.assertToksEqual(word + " ", wordexp)
+		self.assertToksEqual(" " + word, space_exp + word_exp)
+		self.assertToksEqual(word + " ", word_exp + space_exp)
 	def test_text_with_multispace(self):
-		self.assertToksEqual("  " + word, wordexp)
-		self.assertToksEqual(word + "  ", wordexp)
+		self.assertToksEqual("  " + word, space_exp + word_exp)
+		self.assertToksEqual(word + "  ", word_exp + space_exp)
 	def test_text_multiword(self):
-		self.assertToksEqual(word + " " + word, wordexp*2)
-		self.assertToksEqual(" " + word + " " + word + " ", wordexp*2)
+		self.assertToksEqual(word + " " + word, word_exp + space_exp + word_exp)
+		self.assertToksEqual(" " + word + " " + word + " ", (space_exp + word_exp)*2 + space_exp)
 	def test_text_with_newline(self):
-		self.assertToksEqual("\n" + word, wordexp)
-		self.assertToksEqual("\n\n" + word, wordexp)
-		self.assertToksEqual(word + "\n", wordexp)
-	def test_text_with_multiline(self):
-		self.assertToksEqual("\n\n" + word, wordexp)
-		self.assertToksEqual(word + "\n\n", wordexp)
-	def test_multline_text(slf):
-		self.assertToksEqual(word + "\n" + word, wordexp*2)
-		self.assertToksEqual(word + "\n" + word + "\n", wordexp*2)
-		self.assertToksEqual("\n" + word + "\n" + word, wordexp*2)
-		self.assertToksEqual(word + "\n\n" + word, wordexp*2)
+		self.assertToksEqual("\n" + word, space_exp + word_exp)
+		self.assertToksEqual(word + "\n", word_exp + space_exp)
+	def test_multline_text(self):
+		self.assertToksEqual(word + "\n" + word, word_exp + space_exp + word_exp)
+		self.assertToksEqual(word + "\n" + word + "\n", (word_exp + space_exp)*2)
+		self.assertToksEqual("\n" + word + "\n" + word, (space_exp + word_exp)*2)
+
+class TestEndPara(LTC):
+	def test_just_para(self):
+		self.assertToksEqual("\n\n", endpara_exp)
+	def test_far_para(self):
+		self.assertToksEqual("\n\n\n", endpara_exp)
+	def test_para_with_space(self):
+		self.assertToksEqual(" \n\n", endpara_exp)
+		self.assertToksEqual("  \n\n", endpara_exp)
+		self.assertToksEqual("\n\n ", endpara_exp)
+		self.assertToksEqual("\n\n  ", endpara_exp)
+		self.assertToksEqual(" \n\n\n", endpara_exp)
+		self.assertToksEqual("  \n\n\n", endpara_exp)
+	def test_far_para_space_inbetween(self):
+		self.assertToksEqual("\n\n \n\n", endpara_exp)
+		self.assertToksEqual("\n\n   \n\n", endpara_exp)
+	def test_para_with_text(self):
+		self.assertToksEqual(word + "\n\n", word_exp + endpara_exp)
+		self.assertToksEqual("\n\n" + word, endpara_exp + word_exp)
+		self.assertToksEqual("\n\n" + word, endpara_exp + word_exp)
+	def test_multi_para(self):
+		self.assertToksEqual("\n\n" + word + "\n\n", endpara_exp + word_exp + endpara_exp)
+		self.assertToksEqual( word + "\n\n" + word + "\n\n", (word_exp + endpara_exp)*2)
+	def test_para_with_text_and_space(self):
+		self.assertToksEqual(word + " \n\n", word_exp + endpara_exp)
+		self.assertToksEqual(word + "\n\n ", word_exp + endpara_exp)
+		self.assertToksEqual(word + "\n\n \n\n", word_exp + endpara_exp)
+		self.assertToksEqual(" " + word + " \n\n", space_exp + word_exp + endpara_exp)
+		
 
 """
 class TestComment(LTC):
